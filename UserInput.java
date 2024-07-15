d4d2ab3a03ccedd95f548e446cc9cde1733e788d
Rushikore13 <rushikore384@gmail.com>
2024-07-15 10:31:36 +0530

    Add files via upload

---
+++ UserInput.java
@@ -0,0 +1,21 @@
+package demo.ptpl;
+
+public class UserInput { 
+
+	public static void main(String[] args) {
+		
+		        int n = 3; 
+
+		        for (int i = 1; i<=n; i++) {
+		            for (int j = 1; j <=i; j++) {
+		                System.out.print("*");
+		            }
+		            System.out.println();
+		        }
+		    }
+		}
+		
+		       
+		
+	
+
