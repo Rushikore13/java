f39415103679381555f897834cad499b1ddbb9cc
Hrishikesh Kore <rushikore384@gmail.com>
2024-07-16 12:23:43 +0530

    Add files via upload

---
+++ Oops.java
@@ -0,0 +1,14 @@
+package demo.ptpl;
+
+public class Oops {
+	
+	 static void myMethod(String name,int age) {
+		
+         System.out.println("Data  "+name+"  "+age);
+	}
+     public static void main(String[]args) {
+    	 myMethod("Rushi",21);
+    	 myMethod("pranav",20);
+    	 myMethod("Subi",19);
+     }
+}
